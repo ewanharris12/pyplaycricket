648345d1f70dc1f045c049a290540b9ee7f87657
Ewan Harris <54112442+ewanharris12@users.noreply.github.com>
2024-08-23 15:11:57 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(name='pyplaycricket',
       version='0.1.0',
       description='Iteracting with the playcricket API for statistical analysis and automated social media posts.',
       author='Ewan Harris',
-      url='https://github.com/ewanharris12/pyplaycricket'
+      url='https://github.com/ewanharris12/pyplaycricket',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown'
       )
