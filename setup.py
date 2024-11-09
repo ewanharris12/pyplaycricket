b4a83050fab8395c15f6ad2b50cf27b52ac1422c
Ewan Harris <54112442+ewanharris12@users.noreply.github.com>
2024-11-09 20:47:27 +0000

    updating version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyplaycricket',
-      version='0.1.1',
+      version='0.1.2',
       description='Iteracting with the playcricket API for statistical analysis and automated social media posts.',
       author='Ewan Harris',
       url='https://github.com/ewanharris12/pyplaycricket',
