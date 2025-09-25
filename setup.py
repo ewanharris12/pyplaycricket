c91d328696911136f727ffee8c7af0e9dd2b42c3
Ewan Harris <54112442+ewanharris12@users.noreply.github.com>
2025-09-25 12:23:45 +0100

    fixing packages and version number

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='pyplaycricket',
-      version='0.2',
+      version='0.2.1',
       description='Iteracting with the playcricket API for statistical analysis and automated social media posts.',
       author='Ewan Harris',
       url='https://github.com/ewanharris12/pyplaycricket',
     #   long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
-      py_modules=['alleyn', 'utils', 'playcric'],
+      packages=['playcric'],
     #   install_requires=required
       )
